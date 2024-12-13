f763641e65827df2a2e127415a8a8db7da21458d
tushar-454 <imtushar454@gmail.com>
2024-12-13 08:58:57 +0600

    refactor: restructure Profile component layout for improved readability

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -26,56 +26,55 @@ const Profile = ({ isMobile }: ProfileProps) => {
   const { name, email, photo, role } = user || {};
 
   return (
-    <div
-      style={isMobile ? { justifyContent: 'start' } : {}}
-      className={isMobile ? 'flex flex-row-reverse gap-3' : 'flex items-center gap-3'}
-    >
-      <span className={isMobile ? 'flex flex-col items-start' : 'flex flex-col items-end'}>
-        <span className=''>{name || 'Tushar Imran'}</span>
-        <span className='text-xs'>{email || 'tusharimran@gmail.com'}</span>
-      </span>
-
-      <DropdownMenu>
-        <DropdownMenuTrigger>
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <div
+          style={isMobile ? { justifyContent: 'start' } : {}}
+          className={isMobile ? 'flex flex-row-reverse gap-3' : 'flex items-center gap-3'}
+        >
+          <span className={isMobile ? 'flex flex-col items-start' : 'flex flex-col items-end'}>
+            <span className=''>{name || 'Tushar Imran'}</span>
+            <span className='text-xs'>{email || 'tusharimran@gmail.com'}</span>
+          </span>
           <img
             src={photo || ''}
             alt='profile'
             className='size-10 cursor-pointer rounded-full object-cover shadow-xl'
           />
-        </DropdownMenuTrigger>
-        <DropdownMenuContent>
-          <DropdownMenuLabel>My Account</DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          {role === 'admin' && (
-            <DropdownMenuItem>
-              <span className='flex cursor-pointer items-center gap-2'>
-                <MdOutlineSpaceDashboard />
-                <Link to={'/dashboard'}>Dashboard</Link>
-              </span>
-            </DropdownMenuItem>
-          )}
+        </div>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        {role === 'admin' && (
           <DropdownMenuItem>
             <span className='flex cursor-pointer items-center gap-2'>
-              <CiLogout />
-              <span
-                onClick={async () => {
-                  const res = await logout({});
-                  if (res.data.status === 200) {
-                    toast({
-                      title: 'Logout',
-                      description: 'You have successfully logged out',
-                    });
-                    window.location.href = '/login';
-                  }
-                }}
-              >
-                Logout
-              </span>
+              <MdOutlineSpaceDashboard />
+              <Link to={'/dashboard'}>Dashboard</Link>
             </span>
           </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
-    </div>
+        )}
+        <DropdownMenuItem>
+          <span className='flex cursor-pointer items-center gap-2'>
+            <CiLogout />
+            <span
+              onClick={async () => {
+                const res = await logout({});
+                if (res.data.status === 200) {
+                  toast({
+                    title: 'Logout',
+                    description: 'You have successfully logged out',
+                  });
+                  window.location.href = '/login';
+                }
+              }}
+            >
+              Logout
+            </span>
+          </span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 };
 
